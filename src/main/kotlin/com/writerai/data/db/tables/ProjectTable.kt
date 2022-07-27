7e9f6ab32ddf00d1722672ad267912ce6d62cf4a
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-07-27 20:03:47 +0530

    Project character limit increased

--- src/main/kotlin/com/writerai/data/db/tables/ProjectTable.kt
+++ src/main/kotlin/com/writerai/data/db/tables/ProjectTable.kt
@@ -6,8 +6,8 @@ import org.jetbrains.exposed.sql.Column
 object ProjectTable : IntIdTable() {
     val userId: Column<String> = varchar("userId", 50)
     val title: Column<String> = varchar("title", 50)
-    val description: Column<String> = varchar("description", 100)
-    val content: Column<String> = varchar("content", 2000)
-    val coverPic:Column<String> = varchar("coverPic", 200)
+    val description: Column<String> = varchar("description", 1000)
+    val content: Column<String> = varchar("content", 100000)
+    val coverPic:Column<String> = varchar("coverPic", 500)
     val timeStamp: Column<Long> = long("timeStamp")
 }
