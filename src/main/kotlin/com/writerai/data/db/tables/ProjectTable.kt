ab3244651b8e7c6ffe180be929ecc8066871672d
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-07-27 20:42:30 +0530

    Project character limit error fixed

--- src/main/kotlin/com/writerai/data/db/tables/ProjectTable.kt
+++ src/main/kotlin/com/writerai/data/db/tables/ProjectTable.kt
@@ -7,7 +7,7 @@ object ProjectTable : IntIdTable() {
     val userId: Column<String> = varchar("userId", 50)
     val title: Column<String> = varchar("title", 50)
     val description: Column<String> = varchar("description", 1000)
-    val content: Column<String> = varchar("content", 100000)
+    val content: Column<String> = varchar("content", 10000)
     val coverPic:Column<String> = varchar("coverPic", 500)
     val timeStamp: Column<Long> = long("timeStamp")
 }
